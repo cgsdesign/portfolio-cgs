7dd91b57926398c0dd5319d15597e3a279e6dc83
Cat <CGraham@CatherinesMBP2.attlocal.net>
2021-01-31 00:37:38 -0600

    all portfolio pieces brought into json array

---
+++ src/components/Projects/index.js
@@ -0,0 +1,4 @@
+// map over array and insert in the information for the modules
+//create styling for one module
+//create array of information
+        //Image, Title, Link to Deployed Site, Link to Github, (optional description)
